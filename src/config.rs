a37b1976dc8ca4e3a3fbf4b88ca40aad930cfaee
breynard <dev@breynard.net>
2026-06-29 20:02:18 -0300

    Set up config file serialization

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
 use spdlog::error;
-use std::path::Path;
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, Default)]
 pub struct Config {
     pub is_french: bool,
     pub recent_projects: Vec<String>,
@@ -26,6 +25,38 @@ impl Config {
     }
 
     pub fn save_config(&self) {
-        // std::fs::write(Self::get_config_file_location(), toml)
+        match std::fs::write(
+            Self::get_config_file_location(),
+            toml::to_string(self).unwrap(),
+        ) {
+            Ok(_) => {}
+            Err(e) => {
+                error!("Failed to write config file: {e}")
+            }
+        }
+    }
+
+    pub fn load_config() -> Self {
+        let raw_str = match std::fs::read_to_string(Self::get_config_file_location()) {
+            Ok(str) => str,
+            Err(e) => {
+                error!("Failed to read config: {e}");
+                return Self::default();
+            }
+        };
+        let maybe_parse = toml::from_str(&raw_str);
+        maybe_parse.unwrap_or_else(|e| {
+            error!("Failed to parse config file: {e}");
+            Self::default()
+        })
+    }
+
+    pub fn tweak_config<F>(func: F)
+    where
+        F: FnOnce(&mut Self),
+    {
+        let mut config = Self::load_config();
+        func(&mut config);
+        config.save_config()
     }
 }
